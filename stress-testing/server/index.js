089d32471b6479fae0e4a1987cd669d6683b152a
Andrew Francis <apfrancis@gmail.com>
2015-12-09 17:41:11 +0000

    adding prompt functionality

--- stress-testing/server/index.js
+++ stress-testing/server/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Sync = require('sync');
+const prompt = require('prompt');
 const elasticsearchLoader = require('./elasticsearch-loader');
 const client = elasticsearchLoader.getClient();
 
@@ -18,42 +20,133 @@ const percolateOptions = {
     }
 };
 
-const numberOfProfiles = 50000;
 const profilesPerChunk = 20000;
 let run = 0;
 
-elasticsearchLoader.deleteIndices()
-    .then(function(){
-        //console.log('done');
-        return elasticsearchLoader.createNewIndex(indexName)
-    }).then(function(){
-        return elasticsearchLoader.createMapping(indexName,typeName)
-    }).then(function(){
-        console.log('run',run+1);
-        let all = [];
-        if( (numberOfProfiles % profilesPerChunk) === 0 ){
-            let chunks = numberOfProfiles / profilesPerChunk;
-            console.log('dividing',numberOfProfiles,'profiles into',chunks,'chunks of',profilesPerChunk);
-            //console.log(getChunks(10000,chunks));
-            chunks = getChunks(numberOfProfiles,chunks);
-            for(let i=0; i<chunks.length; i++){
-                let offset = chunks[i]*i;
-                //console.log('offset=',offset);
-                all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(chunks[i],sampleDoc,100,{offset:offset}));
-            }
-        } else {
-            all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(numberOfProfiles,sampleDoc,100));
+
+prompt.message = "".rainbow;
+
+var schema = {
+    properties: {
+        remove: {
+            description: 'Do you want to remove any existing ES indicies?',
+            conform: function(value){
+                return (value.toLowerCase() === 'yes' || value.toLowerCase() === 'no')
+            },
+            message: 'Please answer "yes" or "no"',
+            required:true
+        },
+        howManyDocs: {
+            description: 'How many percolator documents would you like to create?',
+            required: true
+        },
+        percolate: {
+            description: 'Do you want to percolate the documents when they have been added?',
+            conform: function(value){
+                return (value.toLowerCase() === 'yes' || value.toLowerCase() === 'no')
+            },
+            message: 'Please answer "yes" or "no"',
+            required:true
         }
-        return Promise.all(all);
-    }).then(function(){
-        percolateOptions.body.doc = sampleDoc;
-        client.percolate(percolateOptions, function (error, response) {
-            if(error) console.log(error);
-            //console.log(response);
-            console.log('percolate took',response.took,'ms');
-            console.log('matched',response.total,'documents');
+    }
+};
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: email, password
+//
+prompt.get(schema, function (err, result) {
+
+    let chain = new Promise(function(resolve, reject) {
+            resolve();
         });
-    }).catch(function(error){console.log(error)});
+
+    // Run in a fiber
+    Sync(function(){
+        try {
+            if(result.remove === 'yes') {
+                chain = chain.then(function(){return elasticsearchLoader.deleteIndices()})
+                             .then(function(){return elasticsearchLoader.createNewIndex(indexName)})
+                             .then(function(){return elasticsearchLoader.createMapping(indexName,typeName)});
+            }
+
+            if(result.howManyDocs) {
+                chain = chain.then(function(){
+                    console.log('run',run+1);
+                    let all = [];
+                    if( (result.howManyDocs % profilesPerChunk) === 0 ){
+                        let chunks = result.howManyDocs / profilesPerChunk;
+                        console.log('dividing',result.howManyDocs,'profiles into',chunks,'chunks of',profilesPerChunk);
+                        //console.log(getChunks(10000,chunks));
+                        chunks = getChunks(result.howManyDocs,chunks);
+                        for(let i=0; i<chunks.length; i++){
+                            let offset = chunks[i]*i;
+                            //console.log('offset=',offset);
+                            all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(chunks[i],sampleDoc,100,{offset:offset}));
+                        }
+                    } else {
+                        all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(result.howManyDocs,sampleDoc,100));
+                    }
+                    return Promise.all(all);
+                });
+            }
+
+            if(result.percolate === 'yes') {
+                chain = chain.then(function(){
+                    percolateOptions.body.doc = sampleDoc;
+                    client.percolate(percolateOptions, function (error, response) {
+                        if(error) console.log(error);
+                        //console.log(response);
+                        console.log('percolate took',response.took,'ms');
+                        console.log('matched',response.total,'documents');
+                    });
+                });
+            }
+        }
+        catch (e) {
+            // If some of async functions returned an error to a callback
+            // it will be thrown as exception
+            console.error(e);
+        }
+    });
+});
+
+//elasticsearchLoader.deleteIndices()
+//    .then(function(){
+//        //console.log('done');
+//        return elasticsearchLoader.createNewIndex(indexName)
+//    }).then(function(){
+//        return elasticsearchLoader.createMapping(indexName,typeName)
+//    }).then(function(){
+//        console.log('run',run+1);
+//        let all = [];
+//        if( (numberOfProfiles % profilesPerChunk) === 0 ){
+//            let chunks = numberOfProfiles / profilesPerChunk;
+//            console.log('dividing',numberOfProfiles,'profiles into',chunks,'chunks of',profilesPerChunk);
+//            //console.log(getChunks(10000,chunks));
+//            chunks = getChunks(numberOfProfiles,chunks);
+//            for(let i=0; i<chunks.length; i++){
+//                let offset = chunks[i]*i;
+//                //console.log('offset=',offset);
+//                all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(chunks[i],sampleDoc,100,{offset:offset}));
+//            }
+//        } else {
+//            all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(numberOfProfiles,sampleDoc,100));
+//        }
+//        return Promise.all(all);
+//    }).then(function(){
+//        percolateOptions.body.doc = sampleDoc;
+//        client.percolate(percolateOptions, function (error, response) {
+//            if(error) console.log(error);
+//            //console.log(response);
+//            console.log('percolate took',response.took,'ms');
+//            console.log('matched',response.total,'documents');
+//        });
+//    }).catch(function(error){console.log(error)});
 
 
 function getChunks(total,chunks){
