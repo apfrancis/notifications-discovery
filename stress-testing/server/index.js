f4605ff00206dd41d262b1d9bfa3e184b0094f8c
Andrew Francis <apfrancis@gmail.com>
2015-12-09 17:42:11 +0000

    tidying up

--- stress-testing/server/index.js
+++ stress-testing/server/index.js
@@ -115,39 +115,6 @@ prompt.get(schema, function (err, result) {
     });
 });
 
-//elasticsearchLoader.deleteIndices()
-//    .then(function(){
-//        //console.log('done');
-//        return elasticsearchLoader.createNewIndex(indexName)
-//    }).then(function(){
-//        return elasticsearchLoader.createMapping(indexName,typeName)
-//    }).then(function(){
-//        console.log('run',run+1);
-//        let all = [];
-//        if( (numberOfProfiles % profilesPerChunk) === 0 ){
-//            let chunks = numberOfProfiles / profilesPerChunk;
-//            console.log('dividing',numberOfProfiles,'profiles into',chunks,'chunks of',profilesPerChunk);
-//            //console.log(getChunks(10000,chunks));
-//            chunks = getChunks(numberOfProfiles,chunks);
-//            for(let i=0; i<chunks.length; i++){
-//                let offset = chunks[i]*i;
-//                //console.log('offset=',offset);
-//                all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(chunks[i],sampleDoc,100,{offset:offset}));
-//            }
-//        } else {
-//            all.push(elasticsearchLoader.createPercolatorQueriesFromDoc(numberOfProfiles,sampleDoc,100));
-//        }
-//        return Promise.all(all);
-//    }).then(function(){
-//        percolateOptions.body.doc = sampleDoc;
-//        client.percolate(percolateOptions, function (error, response) {
-//            if(error) console.log(error);
-//            //console.log(response);
-//            console.log('percolate took',response.took,'ms');
-//            console.log('matched',response.total,'documents');
-//        });
-//    }).catch(function(error){console.log(error)});
-
 
 function getChunks(total,chunks){
     let a;
